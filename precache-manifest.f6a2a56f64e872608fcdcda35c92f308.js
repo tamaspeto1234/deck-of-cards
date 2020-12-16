21d8fff68f4e51a2eeb7ff09a7237c699570f027
tamaspeto1234 <tamas.peto.jobs@gmail.com>
2020-12-16 21:22:22 +0100

    Updates

---
+++ precache-manifest.f6a2a56f64e872608fcdcda35c92f308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd7bb8537ec32e710e02",
+    "url": "/deck-of-cards/static/css/main.cc0b97c6.chunk.css"
+  },
+  {
+    "revision": "dd7bb8537ec32e710e02",
+    "url": "/deck-of-cards/static/js/main.dd7bb853.chunk.js"
+  },
+  {
+    "revision": "a42706de495a6f50032a",
+    "url": "/deck-of-cards/static/js/1.a42706de.chunk.js"
+  },
+  {
+    "revision": "f317f0df2629cb3a97ae",
+    "url": "/deck-of-cards/static/js/runtime~main.f317f0df.js"
+  },
+  {
+    "revision": "8d0071d4957ffa2e8b6b80430b796452",
+    "url": "/deck-of-cards/index.html"
+  }
+];
